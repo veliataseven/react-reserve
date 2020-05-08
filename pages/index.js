b0970a309f9aab8af15ac0d44326bd44b941e472
veliataseven <veliataseven@gmail.com>
2020-05-08 19:43:47 +0200

    Fetching data on the server

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,23 @@
-function Home() {
+import React from 'react';
+import axios from 'axios';
+
+function Home({ products }) {
+  console.log(products);
+  //   React.useEffect(() => {
+  //     getProducts();
+  //   }, []);
+
+  //   const getProducts = async () => {
+  //     const response = await axios('http://localhost:3000/api/products');
+  //     console.log(response.data);
+  //   };
   return <>home</>;
 }
+
+Home.getInitialProps = async () => {
+  const url = 'http://localhost:3000/api/products';
+  const response = await axios(url);
+  return { products: response.data };
+};
 
 export default Home;
