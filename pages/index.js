e931a114509936a5cbb38aee356f06f7528c275e
veliataseven <veliataseven@gmail.com>
2020-06-01 13:27:56 +0200

    Pagination and  Managing User Roles

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,30 @@
+import React from 'react';
 import axios from 'axios';
 import ProductList from '../components/Index/ProductList';
 import baseUrl from '../utils/baseUrl';
+import ProductPagination from '../components/Index/ProductPagination';
 
-function Home({ products }) {
-  return <ProductList products={products} />;
+function Home({ products, totalPages }) {
+  return (
+    <>
+      <ProductList products={products} />
+      <ProductPagination totalPages={totalPages} />
+    </>
+  );
 }
 
-Home.getInitialProps = async () => {
-  // fetch data on server
+// getInitialProps allows the page to wait for data before rendering starts.
+Home.getInitialProps = async (ctx) => {
+  // console.log(ctx.query);
+  const page = ctx.query.page ? ctx.query.page : '1';
+  const size = 9;
+  // fetch data in server
   const url = `${baseUrl}/api/products`;
-  const response = await axios.get(url);
+  const payload = { params: { page, size } };
+  const response = await axios.get(url, payload);
+  return response.data;
   // return response data as an object
-  return { products: response.data };
-  // note: this object will be merged with existing props
+  // note: this object will be merged with existing prop
 };
 
 export default Home;
